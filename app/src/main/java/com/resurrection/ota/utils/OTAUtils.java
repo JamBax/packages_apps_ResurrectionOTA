6482d9354de44dbc9bb72c8bfeeac096e534f4cb
Giuseppe Barillari <joe2k01dev@gmail.com>
2017-12-17 14:27:40 +0100

    ResurrectionOTA: Don't start the intent if there's no browser installed
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- app/src/main/java/com/resurrection/ota/utils/OTAUtils.java
+++ app/src/main/java/com/resurrection/ota/utils/OTAUtils.java
@@ -18,12 +18,14 @@ package com.resurrection.ota.utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.resurrection.ota.configs.OTAConfig;
+import com.resurrection.ota.R;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -105,7 +107,14 @@ public final class OTAUtils {
 
     public static void launchUrl(String url, Context context) {
         if (!url.isEmpty() && context != null) {
-            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            PackageManager pm = context.getPackageManager();
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            if (intent.resolveActivity(pm) != null) {
+                context.startActivity(intent);
+            } else {
+                Toast toast = Toast.makeText(context, R.string.toast_message, Toast.LENGTH_LONG);
+                toast.show();
+            }
         }
     }
 }
